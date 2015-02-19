e63c6e24d21ac3576ae43716938d1a5150047645
torholmslettebak <torholmslettebak@gmail.com>
2015-02-19 06:35:31 +0100

    Fixed sumvec_MPI.c and therefore sumvec_comb changed from scatter gather to send and recv. program now compiles and runs nicely

--- sumvec_MPI.c
+++ sumvec_MPI.c
@@ -10,15 +10,7 @@
 // if you write 2 instead of 4 -> 2 processors
 #define M_PI 3.14159265358979323846
 
-Vector generateVector(int length)
-{
-	Vector vec = createVector(length);
-	for (int i = 0; i < length; i++)
-	{
-		vec -> data[i] = (1.0 / ((i + 1.0)*(i + 1.0)));
-	}
-	return vec;
-}
+
 
 void fillVectorWithSeries(Vector vec)
 {
@@ -28,10 +20,10 @@ void fillVectorWithSeries(Vector vec)
 	}
 }
 
-double sumVector(Vector vec, int length)
+double sumVector(Vector vec, int start, int stop)
 {
 	double sum = 0;
-	for (int i = 0; i < length; i++)
+	for (int i = start; i < stop; i++)
 	{
 		sum += vec -> data[i];	
 	}
@@ -44,7 +36,7 @@ double difference(double sum)
 	return actualSum - sum;
 }
 
-// used for testing
+// used for testing of vectors
 void printVector2(int length, Vector vec)
 {
 	printf("Here comes the vector: \n");
@@ -63,13 +55,16 @@ int isPowerOfTwo (unsigned int x)
 
 int main(int argc, char **argv)
 {
-	int length, myid, nproc;
+	int length, myid, nproc, tag1, tag2, tag3;
+	Vector vec;
 	MPI_Status status;
-	double global_sum;
+	
+	//test();
 	// From this point on every process executes a seperate copy of the program
 	MPI_Init (&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &nproc);
 	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+
 	if (isPowerOfTwo(nproc) != 1)
 	{
 		printf("The number of processes needs to be a power of two.\n");
@@ -81,47 +76,65 @@ int main(int argc, char **argv)
 
 	for (int k = 3; k < 15; k++)
 	{
-		double t1, t2, dt;
+		double t1, t2, dt;	
+		int *len, *displ;
 		t1 = WallTime();
 		length = (int) pow(2, k);
-		Vector vec;
+
+		tag1 = 1;
+		tag2 = 2;
+		tag3 = 3;
 		int elements_per_proc = (int) length/nproc;
-		Vector result = createVector(nproc);
-		Vector recVec = createVector(elements_per_proc);
 		if (myid == 0) // If I am root, generate vector
 		{
+			/* Initialize vector */
 			vec = createVector(length);
-			// vec = generateVector(length);
+			double global_sum = 0;
 			fillVectorWithSeries(vec);
-		}
-		MPI_Scatter(vec -> data, elements_per_proc, MPI_DOUBLE, recVec -> data, elements_per_proc, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-		
-		double local_sum = sumVector(recVec, elements_per_proc);
+			splitVector(length, nproc, &len, &displ);
+			/* Distribute vector to the other processes */
+			for (int i = 1; i < nproc; i++)
+			{
+				MPI_Send(&displ[i], 1, MPI_INT, i, tag1, MPI_COMM_WORLD);
+				MPI_Send((vec -> data), length, MPI_DOUBLE, i, tag3, MPI_COMM_WORLD);
+			}
 
-		MPI_Gather(&local_sum, 1, MPI_DOUBLE, result -> data, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+			/* Receives the local sums from each process*/
+			double receivedSum = 0;
+			for (int i = 1; i < nproc; i++)
+			{
+				MPI_Recv(&receivedSum, 1, MPI_DOUBLE, i, tag2, MPI_COMM_WORLD, &status);
+				global_sum += receivedSum;
+			}
 
-		if(myid == 0)
-		{
-			global_sum = sumVector(result, nproc);
-			printf("The global_sum: %lf\n", global_sum);
+			/*sum own elements*/
+			double localSum = sumVector(vec, 0, elements_per_proc);
+			global_sum = global_sum + localSum;
 			double error = (double) ((M_PI * M_PI / 6) - global_sum);
-			printf("The error for 2^k (k = %d)  	  = %d, elements is error = %lf\n", k,length, error);
-			// t2 = WallTime();
-			// dt = t2 - t1;
-			// freeVector(vec);
-			// printf("The time elapsed for 2^k (k = %d) = %d  elements: dt = %lf\n", k, length, dt);
+			t2 = WallTime();
+			dt = t2 - t1;
+			printf("Number of elements: %d\t\t", length);
+			printf("time:   %0.10f\t", dt);
+			printf("sum:	%0.10f\t", global_sum);
+			printf("error:	%0.10f\n", error);
 		}
 
-		freeVector(result);
-		freeVector(recVec);
-		MPI_Barrier(MPI_COMM_WORLD);
-		// freeVector(vec);
-		// printf("First element of vec: %lf\n", vec->data[0]);
-		// printVector2(vec->len, vec);
-		// printf("Vec of length %d\n", vec->len );
+		else
+		{
+			vec = createVector(length);
+			int localIndex;
+			/* Each process receives local index, where it is to begin summing the vector*/
+			MPI_Recv(&localIndex, elements_per_proc, MPI_DOUBLE, 0, tag1, MPI_COMM_WORLD, &status);
+			/* Each process receives a copy of the vector which is stored in vec*/			
+			MPI_Recv((vec -> data), length, MPI_DOUBLE, 0, tag3, MPI_COMM_WORLD, &status);
+			/* Computes the sum for each subvector*/
+			double localSum = sumVector(vec, localIndex, localIndex + elements_per_proc);
+			/* Returns the computed local sum to master process*/
+			MPI_Send(&localSum, 1, MPI_DOUBLE, 0, tag2, MPI_COMM_WORLD);
+		}
+		freeVector(vec);
 	}
 
-	MPI_Barrier(MPI_COMM_WORLD);
 	MPI_Finalize();
 	return 0;
 }
